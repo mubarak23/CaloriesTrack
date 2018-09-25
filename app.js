012051e734db15343dfa921602df2ca04ef6b390
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-09-25 11:54:45 +0100

    All controller created using the module pattern

---
+++ app.js
@@ -0,0 +1,11 @@
+//Storage Controller
+
+//Item Controller
+const ItemCtrl = (function() {
+  //console.log('Item Controller!!!!');
+})();
+
+//UI Controller
+const UICtrl = (function() {})();
+//App Controller
+const App = (function(ItemCtrl, UICtrl) {})(ItemCtrl, UICtrl);
